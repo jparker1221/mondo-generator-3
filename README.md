0c8b35dd33fee73a63cd3132813a001abba2ea70
jparker1221 <jonparker1221@gmail.com>
2023-02-13 22:32:16 -0500

    finalized javascript

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # mondo-generator-3
+
+## Description
+
+My motivation for this project was to build a password generator that creates a random password for the user based on a series of prompts that determine whether or not to include lowercase letters, uppercase letters, numbers, and special characters.
+
+## Installation
+
+N/A
+
+## Usage
+
+![My webpage includes a navigation bar with links that will take you to the corresponding sections of my portfolio.](./assets/images/screenshot.png)
+
+## Credits
+
+N/A
+
+## License
+
+N/A
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 // Assignment code here
+function generatePassword() {
 // Created arrays for lowercase letters, uppercase letters, numbers, and special characters
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 
+var lengthAsk = prompt("How many characters would you like the password to be? Please enter a number between 8 and 128.")
+// console.log(lengthAsk)
 var lowercaseAsk = confirm("Would you like to include lowercase letters?")
 // console.log(lowercaseAsk)
 var uppercaseAsk = confirm("Would you like to include uppercase letters?")
@@ -12,34 +15,41 @@ var uppercaseAsk = confirm("Would you like to include uppercase letters?")
 var numberAsk = confirm("Would you like to include numbers?")
 // console.log(numberAsk)
 var specialAsk = confirm("Would you like to include special characters?")
-// console.log(lowercaseAsk)
-var lengthAsk = prompt("How many characters would you like the password to be?")
+// console.log(specialAsk)
+console.log(lengthAsk)
+console.log(typeof lengthAsk)
+console.log(parseInt(lengthAsk,2))
+console.log(typeof(parseInt(lengthAsk,2)))
 
-if (typeof lengthAsk != number) {
-  var lengthAsk = prompt("That is not a number. Please enter a number between 8 and 128.")
+if (typeof parseInt(lengthAsk,2) != "number") {
+  var lengthAsk = prompt("Please enter a number between 8 and 128.")
 }
 else if (lengthAsk < 8 || lengthAsk > 128) {
-  var lengthAsk = prompt("Password must be between 8 and 128 characters. Please try again.")
+  var lengthAsk = prompt("Password must be between 8 and 128 characters.")
 }
 
 var passwordArray = []
-function generatePassword() {
+
   if (lowercaseAsk) {
-    passwordArray.concat(lowercase)
+    passwordArray=passwordArray.concat(lowercase)
   }
   if (uppercaseAsk) {
-    passwordArray.concat(uppercase)
+    passwordArray=passwordArray.concat(uppercase)
   }
   if (numberAsk) {
-    passwordArray.concat(number)
+    passwordArray=passwordArray.concat(number)
   }
   if (specialAsk) {
-    passwordArray.concat(special)
-  }
-var password = ""
-  
+    passwordArray=passwordArray.concat(special)
+  }  
+var passwordString = "";
+console.log(passwordArray)
+for (let i = 0; i < lengthAsk; i++) {
+  passwordString += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+console.log(passwordArray[Math.floor(Math.random() * passwordArray.length)])
 }
-
+return passwordString
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
